0959290d92d9b17d7d4347dcf48e332ba2dd7e63
nicholas <nja8161995@gmail.com>
2022-10-24 16:31:20 -0500

    Created and began nav layout

--- dist/main.js
+++ dist/main.js
@@ -9,13 +9,13 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
+/***/ "./src/script/index.js":
+/*!*****************************!*\
+  !*** ./src/script/index.js ***!
+  \*****************************/
 /***/ (() => {
 
-eval("console.log(\"Testing NPM build\");\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+eval("\n\n//# sourceURL=webpack://restaurant-page/./src/script/index.js?");
 
 /***/ })
 
@@ -26,7 +26,7 @@ eval("console.log(\"Testing NPM build\");\n\n//# sourceURL=webpack://restaurant-
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/script/index.js"]();
 /******/ 	
 /******/ })()
 ;
